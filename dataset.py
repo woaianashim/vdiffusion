917c42c44eb18f2161ab6fbbe39fd0f37ef69e33
Popov Pavel <lightmo@ya.ru>
2023-10-01 17:16:19 +0300

    add dataset and fix training loop

--- dataset.py
+++ dataset.py
@@ -1,4 +1,6 @@
 import os
+import torch
+import random
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from torchvision.datasets import Flowers102
@@ -11,3 +13,30 @@ class Flower(Flowers102):
                 transforms.ToTensor()
             ])
         super().__init__(os.path.join(path, "flower"), download=True, transform=transform)
+
+
+class NoiseFlower(Flowers102):
+    def __init__(self, path):
+        transform = transforms.Compose([
+                transforms.CenterCrop(500),
+                transforms.Resize((64, 64)),
+                transforms.ToTensor()
+            ])
+        super().__init__(os.path.join(path, "flower"), download=True, transform=transform)
+
+    def __getitem__(self, index, time=None, noise_steps=100, beta_init=1e-4, beta_final=2e-2):
+        if not time:
+            time = random.randint(1, noise_steps)
+
+        image = super().__getitem__(index)[0]
+        betas = torch.linspace(beta_init, beta_final, noise_steps)
+        alphas = 1. - betas[:time]
+        alphas_hat = alphas.prod()
+
+        epsilon = torch.randn(*image.shape)
+
+        return torch.sqrt(alphas_hat) * image + torch.sqrt(1 - alphas_hat) * epsilon, epsilon, image, time
+
+        
+
+
--- main.py
+++ main.py
@@ -23,23 +23,30 @@ def train(cfg: DictConfig):
         model.load_state_dict(torch.load(cfg.resume))
     optim = torch.optim.AdamW(model.parameters(), lr=1e-4)
 
-    for i in tqdm(range(cfg.epochs)):
+    pbar = tqdm(range(cfg.epochs))
+    for i in pbar:
         model.train()
         dl = torch.utils.data.DataLoader(ds, batch_size=32)
 
+        running_loss = 0
         for data in dl:
             optim.zero_grad()
-            image = data[0].cuda()
-            rec, feats = model(image, torch.zeros(image.shape[0]).cuda())
-            loss = torch.nn.functional.mse_loss(image, rec)
+            image, noise, time = data[0].cuda(), data[1].cuda(), data[3].cuda() 
+            rec, feats = model(image, time)
+            loss = torch.nn.functional.mse_loss(noise, rec)
             loss.backward()
+            running_loss += loss.item()
             optim.step()
+
+        pbar.set_postfix(loss=running_loss / len(dl))
 
         if(i%10==0):
             if "outputs" not in os.listdir("."):
                 os.mkdir("outputs")
             with torch.no_grad():
-                image = ds[0][0].unsqueeze(0).cuda()
+                # x_time, noise, image, time = ds[0][0].unsqueeze(0).cuda(), ds[1][0].unsqueeze(0).cuda(), ds[2][0].unsqueeze(0).cuda(), ds[3][0].cuda()
+                # rec = model(x_time, time)
+                # example = torch.cat([image.squeeze(0), x_time.squeeze(0), x_time - rec])
                 rec = generate(model)
                 sample = rec.transpose(0,1).view(3,-1, 64).cpu()
                 to_pil_image(sample).save(f"outputs/sample_{i}.jpg")
